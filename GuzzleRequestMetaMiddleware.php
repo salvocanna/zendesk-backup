40de42206c37bbb9bb64efcdb2bc91cc42076754
Salvo <salvo.cannamela@gmail.com>
2017-03-20 10:04:10 +0000

    Add GuzzleRequestMetaMiddleware to allow passing params from request to response

---
+++ GuzzleRequestMetaMiddleware.php
@@ -0,0 +1,65 @@
+<?php
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+
+class GuzzleRequestMetaMiddleware
+{
+    /**
+     * @var Callable
+     */
+    protected $nextHandler;
+    /**
+     * @var string
+     */
+    protected $headerName;
+    /**
+     * @param callable $nextHandler
+     * @param string   $headerName  The header name to use for storing effective url
+     */
+    public function __construct(
+        callable $nextHandler,
+        $headerName = 'X-GUZZLE-EFFECTIVE-URL'
+    ) {
+        $this->nextHandler = $nextHandler;
+        $this->headerName = $headerName;
+    }
+    /**
+     * Inject effective-url header into response.
+     *
+     * @param RequestInterface $request
+     * @param array            $options
+     *
+     * @return RequestInterface
+     */
+    public function __invoke(RequestInterface $request, array $options)
+    {
+        $fn = $this->nextHandler;
+        return $fn($request, $options)->then(function (ResponseInterface $response) use ($request, $options) {
+            $response = $response->withAddedHeader($this->headerName, $request->getUri()->__toString());
+
+            foreach ($request->getHeaders() as $key => $value) {
+                if (strpos(strtolower($key), 'x-') === 0) {
+                    $response = $response->withAddedHeader($key, $value);
+                }
+            }
+
+            return $response;
+        });
+    }
+
+    /**
+     * Prepare a middleware closure to be used with HandlerStack
+     *
+     * @param string $headerName The header name to use for storing effective url
+     *
+     * @return \Closure
+     */
+    public static function middleware($headerName = 'X-GUZZLE-EFFECTIVE-URL')
+    {
+        return function (callable $handler) use (&$headerName) {
+            return new static($handler, $headerName);
+        };
+    }
+}
